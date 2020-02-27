2dff0c6d9f864102979fa6fca750929fd8c6af69
Cameron J Leverett <cameron.leverett@yahoo.com>
2020-02-27 19:37:44 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+
+def add_student(student, level)
+  roster[level] ||= []
+      roster[level] << student
+    end
+
+def grade(grade)
+  roster.each do |x, y|
+      if x == grade
+end
+end
+
+def sort
+      end
+    end
